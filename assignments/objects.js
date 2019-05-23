9006071c2a20fb92c25d05824e8ece7c2568274d
lizdoyle <lizdoyle1121@gmail.com>
2019-05-22 22:46:30 -0400

    finished OBJECTS MVP

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,10 @@ const interns = [{
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
+  "speak": function(){
+   return "Hello, my name is";
+  }
 },
 {
   "id": 3,
@@ -47,8 +50,12 @@ const interns = [{
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
-}];
+  "gender": "F",
+  "multiplyNums": function (num1, num2) {
+    return num1 * num2;
+}
+}
+];
 
 console.log(interns);
 // ==== Challenge 2: Reading Object Data ==== 
@@ -67,10 +74,21 @@ console.log(interns[3].name);
 console.log(interns[4].gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+
+//  speak = () => {
+//   return "Hello my name is "
+// };
+
+
+console.log(interns[1].speak(), interns[1].name);
+
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+let antonietta = interns[4];
+
+console.log(antonietta.multiplyNums(3, 4));
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
